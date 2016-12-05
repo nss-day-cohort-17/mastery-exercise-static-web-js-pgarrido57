6bc27ac6a216688a8637f74cac0daceed9c2d6e4
Peter Garrido <peter.garrido57@gmail.com>
2016-12-05 14:11:44 -0600

    tiddy up code spacing

--- main.js
+++ main.js
@@ -2,26 +2,26 @@ var pineHieght = document.getElementById("pineHieght");
 var character = document.getElementById("character");
 var grow = document.getElementById("grow");
 
-  var fraserFir = {
-    height: "",
-    key: "",
-  }
+var fraserFir = {
+  height: "",
+  key: "",
+}
 
-function growTree (){
+function growTree() {
   xmas(fraserFir)
 }
 
 var xmas = function(tree) {
   if (pineHieght.value === "", character.value === "") {
-  alert("Can't leave anything blank! Please fill in both questions.");
-}
+    alert("Can't leave anything blank! Please fill in both questions.");
+  }
   tree.height = pineHieght.value
   tree.key = character.value
   for (var i = 0; i < tree.height; i++) {
-  var design = " ".repeat(tree.height - i);
-  design += tree.key.repeat((2 * i) + 1);
-  console.log(design);
-}
+    var design = " ".repeat(tree.height - i);
+    design += tree.key.repeat((2 * i) + 1);
+    console.log(design);
+  }
 }
 
 pineHieght.addEventListener("keypress", function() {
