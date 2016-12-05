2b0b2e76089cab4ef103562e5ec229ece4b658e6
Peter Garrido <peter.garrido57@gmail.com>
2016-12-05 12:00:32 -0600

    alert working

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var character = document.getElementById("character");
 var grow = document.getElementById("grow");
 
 var xmas = function() {
+  if (pineHieght.value === "", character.value === "") {
+  alert("Can't leave anything blank! Please fill in both questions.");
+}
   var fraserFir = {
     height: pineHieght.value,
     key: character.value,
@@ -11,14 +14,11 @@ var xmas = function() {
 }
 
 for (var i = 0; i < fraserFir.height; i++) {
-  var design = " ".repeat(fraserFir.height - i);
+  var design = "".repeat(fraserFir.height - i);
   design += fraserFir.key.repeat((2 * i) + 1);
   console.log(design)
 }
 
-if (fraserFir.height === " ", fraserFir.key === " ") {
-  alert("Can't leave anything blank! Please fill in both questions.");
-}
 
 pineHieght.addEventListener("keypress", function() {
   if (event.keyCode === 13) {
